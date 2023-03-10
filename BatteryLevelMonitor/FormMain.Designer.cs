c50f686bb9f9b8ef2300245c482cf1b1e105fa60
jagapatr <jagapatr@motorola.com>
2023-03-10 11:05:27 -0300

    First commit

---
+++ BatteryLevelMonitor/FormMain.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace BatteryLevelMonitor
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxIp = new System.Windows.Forms.TextBox();
+            this.textBoxStatus = new System.Windows.Forms.TextBox();
+            this.chartBatteryLevel = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonLed = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBoxInterval = new System.Windows.Forms.ComboBox();
+            this.labelVoltage = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chartBatteryLevel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(455, 30);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(87, 40);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(43, 64);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(46, 13);
+            this.labelStatus.TabIndex = 3;
+            this.labelStatus.Text = "Status...";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Unit IP";
+            // 
+            // textBoxIp
+            // 
+            this.textBoxIp.Location = new System.Drawing.Point(43, 30);
+            this.textBoxIp.Name = "textBoxIp";
+            this.textBoxIp.Size = new System.Drawing.Size(129, 20);
+            this.textBoxIp.TabIndex = 5;
+            this.textBoxIp.Text = "192.168.137.128";
+            // 
+            // textBoxStatus
+            // 
+            this.textBoxStatus.Location = new System.Drawing.Point(12, 469);
+            this.textBoxStatus.Multiline = true;
+            this.textBoxStatus.Name = "textBoxStatus";
+            this.textBoxStatus.Size = new System.Drawing.Size(108, 10);
+            this.textBoxStatus.TabIndex = 6;
+            this.textBoxStatus.Visible = false;
+            // 
+            // chartBatteryLevel
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chartBatteryLevel.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chartBatteryLevel.Legends.Add(legend3);
+            this.chartBatteryLevel.Location = new System.Drawing.Point(12, 110);
+            this.chartBatteryLevel.Name = "chartBatteryLevel";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Battery Level";
+            series3.YValuesPerPoint = 4;
+            this.chartBatteryLevel.Series.Add(series3);
+            this.chartBatteryLevel.Size = new System.Drawing.Size(984, 353);
+            this.chartBatteryLevel.TabIndex = 7;
+            this.chartBatteryLevel.Text = "chartBatteryLevel";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Level (%)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(399, 469);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Time (hh:mm:ss)";
+            // 
+            // buttonLed
+            // 
+            this.buttonLed.Location = new System.Drawing.Point(938, 8);
+            this.buttonLed.Name = "buttonLed";
+            this.buttonLed.Size = new System.Drawing.Size(58, 37);
+            this.buttonLed.TabIndex = 11;
+            this.buttonLed.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(192, 8);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Interval (s)";
+            // 
+            // comboBoxInterval
+            // 
+            this.comboBoxInterval.FormattingEnabled = true;
+            this.comboBoxInterval.Items.AddRange(new object[] {
+            "5",
+            "10",
+            "15",
+            "20",
+            "25",
+            "30",
+            "35",
+            "40",
+            "45",
+            "50",
+            "55",
+            "60"});
+            this.comboBoxInterval.Location = new System.Drawing.Point(195, 30);
+            this.comboBoxInterval.Name = "comboBoxInterval";
+            this.comboBoxInterval.Size = new System.Drawing.Size(53, 21);
+            this.comboBoxInterval.TabIndex = 13;
+            this.comboBoxInterval.Text = "5";
+            // 
+            // labelVoltage
+            // 
+            this.labelVoltage.AutoSize = true;
+            this.labelVoltage.Location = new System.Drawing.Point(829, 274);
+            this.labelVoltage.Name = "labelVoltage";
+            this.labelVoltage.Size = new System.Drawing.Size(85, 13);
+            this.labelVoltage.TabIndex = 14;
+            this.labelVoltage.Text = "Battery Voltage: ";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 491);
+            this.Controls.Add(this.labelVoltage);
+            this.Controls.Add(this.comboBoxInterval);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonLed);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chartBatteryLevel);
+            this.Controls.Add(this.textBoxStatus);
+            this.Controls.Add(this.textBoxIp);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.buttonStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Battery Level Monitor v1.0";
+            ((System.ComponentModel.ISupportInitialize)(this.chartBatteryLevel)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxIp;
+        private System.Windows.Forms.TextBox textBoxStatus;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartBatteryLevel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonLed;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBoxInterval;
+        private System.Windows.Forms.Label labelVoltage;
+    }
+}
+
---
+++ BatteryLevelMonitor/FormMain.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BatteryLevelMonitor
+{
+    public partial class FormMain : Form
+    {
+        System.Windows.Forms.Timer timerLevelChart = new System.Windows.Forms.Timer();
+        public static string ipAddress = string.Empty;
+        string temp = string.Empty;
+        private static DateTime dtInitialCurrentChartTime;
+        private System.Threading.Timer timer;
+        int interval = 0;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+
+            ipAddress = textBoxIp.Text;
+            labelStatus.Text = "Connected to device =>" + ipAddress + ":5555";
+            interval = Convert.ToInt32(comboBoxInterval.Text);
+
+            var startTimeSpan = TimeSpan.Zero;
+            var periodTimeSpan = TimeSpan.FromSeconds(interval);
+            timer = new System.Threading.Timer((obj) =>
+            {
+                startRoutine();
+
+            }, null, startTimeSpan, periodTimeSpan);
+
+            timerLevelChart.Interval = (interval + 4) * 1000;
+            timerLevelChart.Tick += new EventHandler(timerLevelChart_Tick);
+            timerLevelChart.Start();
+        }
+
+        public void startRoutine()
+        {
+            try
+            {
+                ipAddress = textBoxIp.Text;
+                dtInitialCurrentChartTime = DateTime.Now;
+                buttonLed.BackColor = Color.Green;
+                var proc = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "cmd.exe",
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        RedirectStandardInput = true,
+                        CreateNoWindow = true
+                    }
+                };
+                proc.Start();
+
+                var outStream = proc.StandardOutput;
+                var inStream = proc.StandardInput;
+
+                inStream.WriteLine("adb connect " + ipAddress + ":5555");
+                inStream.WriteLine("adb shell dumpsys battery");
+
+                temp = "begin" + Environment.NewLine;
+
+                Task.Run(() =>
+                {
+                    while (true)
+                    {
+                        textBoxStatus.Text += outStream.ReadLine();
+                    }
+                });
+                Thread.Sleep(2000);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error: " + e);
+            }
+            finally
+            {
+                buttonLed.BackColor = Color.Red;
+
+            }
+        }
+        public void setValuesLevel()
+        {
+            temp = textBoxStatus.Text;
+            string tempVoltage = temp;
+            string Battlevel = string.Empty;
+            string BattVoltage = string.Empty;
+
+            try
+            {
+                string regExPattern2 = "voltage" + ":(.*?\\s\\s)";
+                MatchCollection fieldValue2 = Regex.Matches(tempVoltage, regExPattern2, RegexOptions.IgnoreCase);
+
+                foreach (var match in fieldValue2)
+                {
+                    BattVoltage = match.ToString();
+                }
+
+                BattVoltage = BattVoltage.Replace("voltage:", "");
+                labelVoltage.Text = "Battery Voltage:" + BattVoltage + "V";
+
+                DateTime today = DateTime.Now;
+                string time = today.ToString("hh:mm:ss");
+                double diffInSeconds = (DateTime.Now - dtInitialCurrentChartTime).TotalSeconds;
+                string regExPattern = "level" + ":(.*?\\s\\s)";
+                Match fieldValue = Regex.Match(temp, regExPattern, RegexOptions.IgnoreCase);
+                Battlevel = fieldValue.Groups[1].Value.Trim();            
+
+                chartBatteryLevel.Series[0].Points.AddXY(time, Battlevel);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error: " + e);
+            }
+        }
+        void timerLevelChart_Tick(object sender, EventArgs e)
+        {
+            setValuesLevel();
+        }
+    }
+}
